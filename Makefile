1eb9c68e73895fdcfd4e9eeccd85081b6b146c17
JohnsonClayton <cpjohnson@mavs.coloradomesa.edu>
2020-02-26 10:23:04 -0700

    initial commit

---
+++ Makefile
@@ -0,0 +1,4 @@
+makefile:
+
+copier: clay_copy.c
+	gcc -g clay_copy.c -o clay_copy
---
+++ clay_copy.c
@@ -0,0 +1,11 @@
+/*
+*  Clayton Johnson
+*  cpjohnson@mavs.coloradomesa.edu
+*
+*  Copy a File
+*/
+
+int main(void) {
+  puts("Hello, world!");
+  return 0;
+}
