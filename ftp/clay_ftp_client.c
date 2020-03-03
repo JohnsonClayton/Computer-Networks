624ab235e6fae906f4fb24f3d4dd4e91ec8d8443
JohnsonClayton <cpjohnson@mavs.coloradomesa.edu>
2020-03-02 21:18:16 -0700

    completed bare minimum for ftp assignment

--- ftp/clay_ftp_client.c
+++ ftp/clay_ftp_client.c
@@ -7,44 +7,54 @@
 #include <sys/socket.h> 
 #include <arpa/inet.h> 
 #include <netinet/in.h> 
+#include <fcntl.h>
 
 #define PORT	 8080 
 #define MAXLINE 1024 
 
+const int MAX_RW = 1024;
+
+void cj_ftp(int sockfd, char *hello, struct sockaddr_in servaddr, char *buffer) {
+  int n, len, bytes_recv; 
+  void *buf[MAX_RW];
+
+  sendto(sockfd, (char*)buffer, 0, MSG_WAITALL, ( struct sockaddr*) &servaddr, sizeof(servaddr));
+
+  // Get file descriptor for where we are copying the incoming data
+  buffer = "test1.copy";
+  int fd = open(buffer, O_WRONLY | O_CREAT, 0666);
+
+  // Receive bytes from server
+  bytes_recv = MAX_RW;
+  while( (bytes_recv >= 1024) && (bytes_recv = recvfrom(sockfd, buf, MAX_RW, MSG_WAITALL, (struct sockaddr *) &servaddr, &len)) > 0 ) {
+    write(fd, buf, bytes_recv);
+  } 
+}
+
 // Driver code 
 int main() { 
-	int sockfd; 
-	char buffer[MAXLINE]; 
-	char *hello = "Hello from client"; 
-	struct sockaddr_in	 servaddr; 
+  int sockfd; 
+  char buffer[MAXLINE]; 
+  char *hello = "Hello from client"; 
+  struct sockaddr_in servaddr; 
 
-	// Creating socket file descriptor 
-	if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) { 
-		perror("socket creation failed"); 
-		exit(EXIT_FAILURE); 
-	} 
+  // Creating socket file descriptor 
+  if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) { 
+    perror("socket creation failed"); 
+    exit(EXIT_FAILURE); 
+  } 
 
-	memset(&servaddr, 0, sizeof(servaddr)); 
-	
-	// Filling server information 
-	servaddr.sin_family = AF_INET; 
-	servaddr.sin_port = htons(PORT); 
-	servaddr.sin_addr.s_addr = INADDR_ANY; 
-	
-	int n, len; 
-	
-	sendto(sockfd, (const char *)hello, strlen(hello), 
-		MSG_CONFIRM, (const struct sockaddr *) &servaddr, 
-			sizeof(servaddr)); 
-	printf("Hello message sent.\n"); 
-		
-	n = recvfrom(sockfd, (char *)buffer, MAXLINE, 
-				MSG_WAITALL, (struct sockaddr *) &servaddr, 
-				&len); 
-	buffer[n] = '\0'; 
-	printf("Server : %s\n", buffer); 
+  memset(&servaddr, 0, sizeof(servaddr)); 
 
-	close(sockfd); 
-	return 0; 
+  // Filling server information 
+  servaddr.sin_family = AF_INET; 
+  servaddr.sin_port = htons(PORT); 
+  servaddr.sin_addr.s_addr = INADDR_ANY; 
+
+  // Copy the data over to a new file here
+  cj_ftp(sockfd, hello, servaddr, buffer);
+
+  close(sockfd); 
+  return 0; 
 } 
 
--- ftp/clay_ftp_server.c
+++ ftp/clay_ftp_server.c
@@ -7,53 +7,66 @@
 #include <sys/socket.h> 
 #include <arpa/inet.h> 
 #include <netinet/in.h> 
+#include <fcntl.h>
 
 #define PORT	 8080 
 #define MAXLINE 1024 
 
+const int MAX_RW = 1024;
+
+void cj_ftp(int sockfd, char *hello, struct sockaddr_in cliaddr, char *buffer) {
+  int len, n; 
+  void *buf[MAX_RW];
+
+  len = sizeof(cliaddr);  //len is value/resuslt 
+
+  n = recvfrom(sockfd, (char *)buffer, MAXLINE, MSG_WAITALL, ( struct sockaddr *) &cliaddr, &len); 
+  //buffer[n] = '\0'; 
+
+  // Get file descriptor for the file to send 
+  buffer = "test1";
+  int fd = open(buffer, O_RDONLY);
+  
+  int size_read;
+
+  // Send bytes to client in MAX_RW chunks
+  while( (size_read = read(fd, buf, MAX_RW)) > 0 ) {
+    sendto(sockfd, (const void *)buf, size_read, MSG_CONFIRM, (const struct sockaddr *) &cliaddr, len); 
+  }
+  //printf("Client : %s\n", buffer); 
+  //printf("Hello message sent.\n");  
+}
+
 // Driver code 
 int main() { 
-	int sockfd; 
-	char buffer[MAXLINE]; 
-	char *hello = "Hello from server"; 
-	struct sockaddr_in servaddr, cliaddr; 
-	
-	// Creating socket file descriptor 
-	if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) { 
-		perror("socket creation failed"); 
-		exit(EXIT_FAILURE); 
-	} 
-	
-	memset(&servaddr, 0, sizeof(servaddr)); 
-	memset(&cliaddr, 0, sizeof(cliaddr)); 
-	
-	// Filling server information 
-	servaddr.sin_family = AF_INET; // IPv4 
-	servaddr.sin_addr.s_addr = INADDR_ANY; 
-	servaddr.sin_port = htons(PORT); 
-	
-	// Bind the socket with the server address 
-	if ( bind(sockfd, (const struct sockaddr *)&servaddr, 
-			sizeof(servaddr)) < 0 ) 
-	{ 
-		perror("bind failed"); 
-		exit(EXIT_FAILURE); 
-	} 
-	
-	int len, n; 
+  int sockfd; 
+  char buffer[MAXLINE]; 
+  char *hello = "Hello from server"; 
+  struct sockaddr_in servaddr, cliaddr; 
 
-	len = sizeof(cliaddr); //len is value/resuslt 
+  // Creating socket file descriptor 
+  if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) { 
+    perror("socket creation failed"); 
+    exit(EXIT_FAILURE); 
+  } 
 
-	n = recvfrom(sockfd, (char *)buffer, MAXLINE, 
-				MSG_WAITALL, ( struct sockaddr *) &cliaddr, 
-				&len); 
-	buffer[n] = '\0'; 
-	printf("Client : %s\n", buffer); 
-	sendto(sockfd, (const char *)hello, strlen(hello), 
-		MSG_CONFIRM, (const struct sockaddr *) &cliaddr, 
-			len); 
-	printf("Hello message sent.\n"); 
-	
-	return 0; 
+  memset(&servaddr, 0, sizeof(servaddr)); 
+  memset(&cliaddr, 0, sizeof(cliaddr)); 
+
+  // Filling server information 
+  servaddr.sin_family = AF_INET; // IPv4 
+  servaddr.sin_addr.s_addr = INADDR_ANY; 
+  servaddr.sin_port = htons(PORT); 
+
+  // Bind the socket with the server address 
+  if ( bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr)) < 0 ) 
+  { 
+    perror("bind failed"); 
+    exit(EXIT_FAILURE); 
+  } 
+
+  cj_ftp(sockfd, hello, cliaddr, buffer);
+
+  return 0; 
 } 
 
